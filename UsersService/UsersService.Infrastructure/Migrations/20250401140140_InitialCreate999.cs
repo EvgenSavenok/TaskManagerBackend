b6f5bc53327bbc66fb3580e2011a40a08dce7b0d
EvgenSavenok <eugen.savenok@gmail.com>
2025-04-03 18:06:25 +0300

    Feat: migration's files

---
+++ UsersService/UsersService.Infrastructure/Migrations/20250401140140_InitialCreate999.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace UsersService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate999 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b68121f1-dfd9-46e8-9031-d9c47f16b41b");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "d613e465-4a94-4156-8720-73e8c8b7fc08");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "1", null, "User", "USER" },
+                    { "2", null, "Administrator", "ADMINISTRATOR" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "b68121f1-dfd9-46e8-9031-d9c47f16b41b", null, "Administrator", "ADMINISTRATOR" },
+                    { "d613e465-4a94-4156-8720-73e8c8b7fc08", null, "User", "USER" }
+                });
+        }
+    }
+}
